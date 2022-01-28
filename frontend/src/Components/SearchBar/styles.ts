f43da4a507e1b8370e33a75eb1b24504fe650351
Verdant31 <joao.pedro3110@hotmail.com>
2022-01-28 18:42:55 -0300

    Final changes

--- frontend/src/Components/SearchBar/styles.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const styles = StyleSheet.create({
-    searchBarStyle: {
-        backgroundColor: 'white',
-        width: '80%',
-        marginTop: 20,
-    }
-});
--- frontend/src/Services/api.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const baseUrl = process.env['API_URL']
-export const api = axios.create({
-  baseURL: 'http://192.168.100.6:3001',
-})
